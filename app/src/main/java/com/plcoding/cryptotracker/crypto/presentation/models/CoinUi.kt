1734ac992c1fcd3420a08ecc4628636ea0ef5f0c
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2025-06-27 19:35:57 +0300

    add Coin.toCoinUi() mapper function

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -1,6 +1,10 @@
 package com.plcoding.cryptotracker.crypto.presentation.models
 
 import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.models.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale // ✅ Add this
 
 data class CoinUi(
     val id: String,
@@ -16,3 +20,26 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+
+fun Coin.toCoinUi() = CoinUi(
+    id = id,
+    rank = rank,
+    name = name,
+    symbol = symbol,
+    princeUsd = princeUsd.toDisplayableNumber(),
+    marketCapUsd = marketCapUsd.toDisplayableNumber(),
+    changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+    iconRes = getDrawableIdForCoin(symbol)
+    )
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        maximumFractionDigits = 2
+    }
+
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
