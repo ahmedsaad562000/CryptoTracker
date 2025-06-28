6fee9f732b42f60f61f7865442e7db71117bf302
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2025-06-28 22:42:03 +0300

    remove unused dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,6 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.gson)
-
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.compose)
     debugImplementation(libs.bundles.compose.debug)
