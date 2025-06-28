2c855e61bb4d937eaf343666cdffb56394aed3ea
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2025-06-29 01:30:01 +0300

    remove unused imports

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListShimmer.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListShimmer.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
-import com.plcoding.cryptotracker.R
 
 @Composable
 fun CoinListShimmer(modifier: Modifier = Modifier) {
