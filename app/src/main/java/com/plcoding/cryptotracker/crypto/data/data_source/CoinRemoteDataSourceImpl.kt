f4ad663b46be65fae9b9330dbce441acece3866d
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2025-06-28 22:38:04 +0300

    add vs_currency paramter to api call

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/data_source/CoinRemoteDataSourceImpl.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/data_source/CoinRemoteDataSourceImpl.kt
@@ -5,20 +5,27 @@ import com.plcoding.cryptotracker.core.data.networking.safeCall
 import com.plcoding.cryptotracker.core.domain.util.NetworkError
 import com.plcoding.cryptotracker.core.domain.util.Result
 import com.plcoding.cryptotracker.core.domain.util.map
-import com.plcoding.cryptotracker.crypto.data.dto.CoinsResponseDto
+import com.plcoding.cryptotracker.crypto.data.dto.CoinDto
 import com.plcoding.cryptotracker.crypto.data.mappers.toCoin
 import com.plcoding.cryptotracker.crypto.domain.models.Coin
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
 
 class CoinRemoteDataSourceImpl(private val httpClient: HttpClient) : CoinRemoteDataSource {
+
+
     override suspend fun getCoins(): Result<List<Coin>, NetworkError> {
-        return safeCall<CoinsResponseDto> {
+        return safeCall<List<CoinDto>> {
             httpClient.get(
-                urlString = constructUrl("/assets")
+                urlString = constructUrl("/markets")
             )
+            {
+                parameter(key = "vs_currency", value = "usd")
+            }
         }.map { response ->
-            response.data.map { coinDto ->
+            response.map { coinDto ->
                 coinDto.toCoin()
             }
         }
