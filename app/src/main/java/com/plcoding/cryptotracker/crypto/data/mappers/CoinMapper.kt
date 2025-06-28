8b189a585f272df117c5e63664d81aec9dd515b7
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2025-06-28 22:37:40 +0300

    make all fields in Dto nullable and use Serial Names for convenient naming

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
@@ -5,12 +5,12 @@ import com.plcoding.cryptotracker.crypto.domain.models.Coin
 
 fun CoinDto.toCoin(): Coin {
     return Coin(
-        id = id,
-        rank = rank,
-        name = name,
-        symbol = symbol,
-        princeUsd = princeUsd,
-        marketCapUsd = marketCapUsd,
-        changePercent24Hr = changePercent24Hr
+        id = id ?: "",
+        rank = marketCapRank ?: 0,
+        name = name ?: "",
+        symbol = symbol ?: "",
+        princeUsd = currentPrice ?: 0.0,
+        marketCapUsd = marketCap ?: 0.0,
+        changePercent24Hr = priceChangePercentage24h ?: 0.0
     )
 }
