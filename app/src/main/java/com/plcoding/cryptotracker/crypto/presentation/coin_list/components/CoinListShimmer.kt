24f31d3894c1daef41296badc7d4a89cbdfa9675
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2025-06-29 13:39:19 +0300

    use lazy column in CoinListShimmer effect

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListShimmer.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListShimmer.kt
@@ -1,9 +1,9 @@
 package com.plcoding.cryptotracker.crypto.presentation.coin_list.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,20 +11,15 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun CoinListShimmer(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+    LazyColumn(
+        modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        (1..10).map {
+        items(15) {
             CoinListItemShimmer(
-                modifier = Modifier
-                    .fillMaxWidth()
+                modifier = Modifier.fillMaxWidth()
             )
             HorizontalDivider()
-
         }
-
-
     }
 
 }
