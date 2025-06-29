486bb3864df732172a4a3cab88994ae3fd198615
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2025-06-29 13:40:36 +0300

    implement pull-to-refresh functionality and display error messages in CoinListScreen

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/view/CoinListScreen.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/view/CoinListScreen.kt
@@ -1,54 +1,92 @@
 package com.plcoding.cryptotracker.crypto.presentation.coin_list.view
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListState
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.plcoding.cryptotracker.core.presentation.util.toString
+import com.plcoding.cryptotracker.crypto.presentation.coin_list.action.CoinListAction
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.CoinListShimmer
-import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.previewCoin
+import com.plcoding.cryptotracker.crypto.presentation.coin_list.view_model.CoinListViewModel
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
+import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CoinListScreen(
-    state: CoinListState,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: CoinListViewModel = koinViewModel<CoinListViewModel>(),
 ) {
-    if (state.isLoading) {
-        CoinListShimmer(
-            modifier = modifier.fillMaxSize(),
-        )
+    val state = viewModel.state.collectAsStateWithLifecycle().value
+    val context = LocalContext.current
+    val pullRefreshState = rememberPullToRefreshState()
+    PullToRefreshBox(
+        isRefreshing = state.isRefreshing,
+        onRefresh = { viewModel.sendAction(CoinListAction.onRefresh) },
+        modifier = modifier,
+        state = pullRefreshState,
+        indicator = {
+            Indicator(
+                modifier = Modifier.align(Alignment.TopCenter),
+                isRefreshing = state.isRefreshing,
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                state = pullRefreshState
+            )
+        }
+    ) {
 
-    } else {
-        LazyColumn(
-            modifier = modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(state.coins)
-            { coin ->
+        if (state.isLoading) {
+            CoinListShimmer(
+                modifier = modifier.fillMaxSize(),
+            )
 
-                CoinListItem(
-                    coinUi = coin,
-                    onClick = {/* TODO */ },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                HorizontalDivider()
+
+        } else {
+            LazyColumn(
+                modifier = modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(state.coins)
+                { coin ->
+
+                    CoinListItem(
+                        coinUi = coin,
+                        onClick = {/* TODO */ },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    HorizontalDivider()
+                }
+
             }
 
         }
 
+        if (state.error != null) {
+            Toast.makeText(
+                context,
+                state.error.toString(context),
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
-
-
 }
 
 @PreviewLightDark
@@ -56,11 +94,7 @@ fun CoinListScreen(
 private fun CoinListPreview() {
     CryptoTrackerTheme {
         CoinListScreen(
-            state = CoinListState(
-                coins = (1..100).map {
-                    previewCoin.copy(id = it.toString())
-                }
-            ),
+            viewModel = koinViewModel<CoinListViewModel>(),
             modifier = Modifier.background(MaterialTheme.colorScheme.background)
         )
     }
