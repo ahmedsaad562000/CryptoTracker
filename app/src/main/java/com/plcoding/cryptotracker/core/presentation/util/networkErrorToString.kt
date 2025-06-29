6337338057099692e5c896b9c38a764fbf0f03c9
Ahmed Saad <ahmed.moh.saad.hussein.ali@gmail.com>
2025-06-29 13:39:48 +0300

    add utility function to convert network errors to string

---
+++ app/src/main/java/com/plcoding/cryptotracker/core/presentation/util/networkErrorToString.kt
@@ -0,0 +1,18 @@
+package com.plcoding.cryptotracker.core.presentation.util
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import com.plcoding.cryptotracker.R
+import com.plcoding.cryptotracker.core.domain.util.NetworkError
+
+fun NetworkError.toString(context: Context): String {
+    val resId = when (this) {
+        NetworkError.NO_INTERNET -> R.string.no_internet_error
+        NetworkError.SERIALIZATION -> R.string.serialization_error
+        NetworkError.REQUEST_TIMEOUT -> R.string.request_timeout_error
+        NetworkError.TOO_MANY_REQUESTS -> R.string.too_many_requests_error
+        NetworkError.SERVER_ERROR -> R.string.server_error
+        NetworkError.UNKNOWN -> R.string.unknown_error
+    }
+    return context.getString(resId)
+}
